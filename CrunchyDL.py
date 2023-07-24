a0726a779399aa9b39bd5f5b463c7f5137cf8fd1
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2023-07-24 11:40:39 -0700

    Add files via upload

--- CrunchyDL.py
+++ CrunchyDL.py
@@ -1,18 +1,31 @@
 import subprocess
 import os
-import threading
-import time
+import threading #implement later
+import time #debug for time
 
+from selenium import webdriver
+
+agent = ''
 def main():
     browser = input("Enter Default Browser: ")
-    agent = input("Enter User Agent of browser: ")
+    
+    with open('uagent.txt', 'r') as text:
+        agent = text.readline()
+        text.close()
+
+
     output = input("Enter path to save files: ")
     input("Crunchyroll is using 30 min cookie rule, please go to crunchyroll and run 1 vid to refresh the cookie. Once you done this you may proceed to download")
     downloader(browser, agent, output) 
 
+
+
+
+
+ 
+
 def downloader(browser, agent, output):
     with open(os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop','Queue.txt') , 'r+') as file:
-    
         while True:
         
             link = file.readline() #using file method
@@ -26,12 +39,30 @@ def downloader(browser, agent, output):
         
             #standard stuff
             #cmd = "yt --embed-sub -f b --cookies-from-browser " + browser  + " --merge-output-format mkv "
-            cmd = "yt --embed-sub --user-agent \"" + agent + "\" --extractor-args crunchyrollbeta:multitrack_adaptive_hls_v2 --cookies-from-browser " + browser  + " --merge-output-format mkv "
+            cmd = "yt --embed-sub --user-agent \"" + agent + "\" --extractor-args crunchyrollbeta:ua_workaround --cookies-from-browser " + browser  + " --merge-output-format mkv "
         
             #desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')    #For desktop Storing
 
             os.chdir(output)
             subprocess.run(cmd + link, shell=True, capture_output=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
