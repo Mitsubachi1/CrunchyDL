bd3005df3e1b7c97ea731c137e528cea78f19f57
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2023-07-30 19:37:28 -0700

    Experimental
    added a queue definition to save whats on text file and split it in half to two different lists
    also added asyncio module to learn how to do multiple tasks, i wanna figure out how to run multiple processes

--- CrunchyDL.py
+++ CrunchyDL.py
@@ -1,11 +1,11 @@
-import subprocess
-import os
-import asyncio
+import subprocess, os
+import asyncio #learning how to use this module (ﾉ´･ω･)ﾉ ﾐ ┸━┸
 import time #debug for time
 
 agent = ''
 
-async def downloader(browser, agent, output):
+async def downloader(browser, agent, output, queue):
+    """                                         Deprecated Code
     with open('Queue.txt' , 'r+') as file:
         while True:
         
@@ -25,21 +25,50 @@ async def downloader(browser, agent, output):
 
             os.chdir(output)
             subprocess.run(cmd + link, shell=True, capture_output=False)
+    """
+    os.chdir(output)
+    for link in queue:
+        cmd = "yt --embed-sub --user-agent \"" + agent + "\" --extractor-args crunchyrollbeta:ua_workaround --cookies-from-browser " + browser  + " --merge-output-format mkv "
+        subprocess.run(cmd + link, shell=True, capture_output=False)
+        return "EOF. Download list complete"
+    
+
+async def queue(browser, agent, output):
+    with open('Queue.txt' , 'r+') as file:
+        queue, second_queue = [], []
+        link = file.readline()
+        
+        while link != '':
+            queue.append(link)
+            link = file.readline()
+        file.close()
+
+        if (len(queue) % 2) == 0: #split list in half, proceed to download
+            splitIndex = len(queue) // 2
+            second_queue = queue[splitIndex:]
+            queue = queue[:splitIndex]
+            task = [downloader(browser, agent, output, queue), downloader(browser, agent, output, second_queue)]
+            await asyncio.gather(*task)
+        print(queue)
+        print(second_queue)
+
+        
+
+        
+                
 
 async def main():
+    #Setup basic args for util
     browser = input("Enter Default Browser: ")
-    
     with open('uagent.txt', 'r') as text:
         agent = text.readline()
         text.close()
-
-
     output = input("Enter path to save files: ")
+    #Advise User
     input("Crunchyroll is using 30 min cookie rule, please go to crunchyroll and run 1 vid to refresh the cookie. Once you done this you may proceed to download")
-    result = await downloader(browser, agent, output)
-    print(result) 
 
-
+    await queue(browser, agent, output)
+     
 
 
 if __name__ == "__main__":
